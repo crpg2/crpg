343cad6a1a8c376a49925e7a027a60e0df65f8c9
Yeldur <haotianyeldur@gmail.com>
2025-03-15 02:47:06 +0000

    add opacity and styling to button

--- src/WebUI/src/pages/characters/[id]/characteristic.vue
+++ src/WebUI/src/pages/characters/[id]/characteristic.vue
@@ -325,111 +325,114 @@ onBeforeRouteUpdate(() => {
           />
         </ConfirmActionTooltip>
         
-        <Modal :disabled="!respecCapability.enabled">
-          <VTooltip placement="auto">
-            <OButton
-              variant="secondary"
-              size="lg"
-              :disabled="!respecCapability.enabled"
-              icon-left="chevron-down-double"
-              data-aq-character-action="respecialize"
-            >
-              <div class="flex items-center gap-2">
-                <span>{{ $t('character.settings.respecialize.title') }}</span>
-                
-                <Tag
-                  v-if="respecCapability.price === 0"
-                  variant="success"
-                  size="sm"
-                  label="free"
-                />
-                
-                <Coin v-else />
-              </div>
-            </OButton>
+        <div class="relative">
+          <Modal :disabled="!respecCapability.enabled">
+            <VTooltip placement="auto">
+              <OButton
+                variant="info"
+                size="lg"
+                :disabled="!respecCapability.enabled"
+                icon-left="chevron-down-double"
+                data-aq-character-action="respecialize"
+                class="ring-1 ring-blue-500"
+              >
+                <div class="flex items-center gap-2">
+                  <span>{{ $t('character.settings.respecialize.title') }}</span>
+                  
+                  <Tag
+                    v-if="respecCapability.price === 0"
+                    variant="success"
+                    size="sm"
+                    label="free"
+                  />
+                  
+                  <Coin v-else />
+                </div>
+              </OButton>
 
-            <template #popper>
-              <div class="prose prose-invert">
-                <h5>{{ $t('character.settings.respecialize.tooltip.title') }}</h5>
-                <div
-                  v-html="
-                    $t('character.settings.respecialize.tooltip.desc', {
-                      freeRespecPostWindow: $t('dateTimeFormat.hh', {
-                        hours: freeRespecializePostWindowHours,
-                      }),
-                      freeRespecInterval: $t('dateTimeFormat.dd', {
-                        days: freeRespecializeIntervalDays,
-                      }),
-                    })
-                  "
-                />
-
-                <div
-                  v-if="respecCapability.freeRespecWindowRemain > 0"
-                  v-html="
-                    $t('character.settings.respecialize.tooltip.freeRespecPostWindowRemaining', {
-                      remainingTime: $t('dateTimeFormat.dd:hh:mm', {
-                        ...parseTimestamp(respecCapability.freeRespecWindowRemain),
-                      }),
-                    })
-                  "
-                />
-
-                <template v-else-if="respecCapability.price > 0">
-                  <i18n-t
-                    scope="global"
-                    keypath="character.settings.respecialize.tooltip.paidRespec"
-                    tag="p"
-                  >
-                    <template #respecPrice>
-                      <Coin :value="respecCapability.price" />
-                    </template>
-                  </i18n-t>
-
+              <template #popper>
+                <div class="prose prose-invert">
+                  <h5>{{ $t('character.settings.respecialize.tooltip.title') }}</h5>
                   <div
                     v-html="
-                      $t('character.settings.respecialize.tooltip.freeRespecIntervalNext', {
-                        nextFreeAt: $t('dateTimeFormat.dd:hh:mm', {
-                          ...parseTimestamp(respecCapability.nextFreeAt),
+                      $t('character.settings.respecialize.tooltip.desc', {
+                        freeRespecPostWindow: $t('dateTimeFormat.hh', {
+                          hours: freeRespecializePostWindowHours,
+                        }),
+                        freeRespecInterval: $t('dateTimeFormat.dd', {
+                          days: freeRespecializeIntervalDays,
                         }),
                       })
                     "
                   />
-                </template>
-              </div>
-            </template>
-          </VTooltip>
 
-          <template #popper="{ hide }">
-            <ConfirmActionForm
-              :title="$t('character.settings.respecialize.dialog.title')"
-              :name="character.name"
-              :confirm-label="$t('action.apply')"
-              @cancel="hide"
-              @confirm="
-                () => {
-                  onRespecializeCharacter();
-                  hide();
-                }
-              "
-            >
-              <template #description>
-                <i18n-t
-                  scope="global"
-                  keypath="character.settings.respecialize.dialog.desc"
-                  tag="p"
-                >
-                  <template #respecializationPrice>
-                    <Coin
-                      :value="respecCapability.price"
-                      :class="{ 'text-status-danger': respecCapability.price > 0 }"
+                  <div
+                    v-if="respecCapability.freeRespecWindowRemain > 0"
+                    v-html="
+                      $t('character.settings.respecialize.tooltip.freeRespecPostWindowRemaining', {
+                        remainingTime: $t('dateTimeFormat.dd:hh:mm', {
+                          ...parseTimestamp(respecCapability.freeRespecWindowRemain),
+                        }),
+                      })
+                    "
+                  />
+
+                  <template v-else-if="respecCapability.price > 0">
+                    <i18n-t
+                      scope="global"
+                      keypath="character.settings.respecialize.tooltip.paidRespec"
+                      tag="p"
+                    >
+                      <template #respecPrice>
+                        <Coin :value="respecCapability.price" />
+                      </template>
+                    </i18n-t>
+
+                    <div
+                      v-html="
+                        $t('character.settings.respecialize.tooltip.freeRespecIntervalNext', {
+                          nextFreeAt: $t('dateTimeFormat.dd:hh:mm', {
+                            ...parseTimestamp(respecCapability.nextFreeAt),
+                          }),
+                        })
+                      "
                     />
                   </template>
-                </i18n-t>
+                </div>
               </template>
-            </ConfirmActionForm>
-          </template>
-        </Modal>
+            </VTooltip>
+
+            <template #popper="{ hide }">
+              <ConfirmActionForm
+                :title="$t('character.settings.respecialize.dialog.title')"
+                :name="character.name"
+                :confirm-label="$t('action.apply')"
+                @cancel="hide"
+                @confirm="
+                  () => {
+                    onRespecializeCharacter();
+                    hide();
+                  }
+                "
+              >
+                <template #description>
+                  <i18n-t
+                    scope="global"
+                    keypath="character.settings.respecialize.dialog.desc"
+                    tag="p"
+                  >
+                    <template #respecializationPrice>
+                      <Coin
+                        :value="respecCapability.price"
+                        :class="{ 'text-status-danger': respecCapability.price > 0 }"
+                      />
+                    </template>
+                  </i18n-t>
+                </template>
+              </ConfirmActionForm>
+            </template>
+          </Modal>
+        </div>
       </div>
     </div>
   </div>
