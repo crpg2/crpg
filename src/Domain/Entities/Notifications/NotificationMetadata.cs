0c6c1b60dedaafd97aaf4c062649c45e72a36950
Valeriy Sinevich <zohan33333@gmail.com>
2025-04-09 23:19:22 +0000

    user notifications

---
+++ src/Domain/Entities/Notifications/NotificationMetadata.cs
@@ -0,0 +1,14 @@
+﻿namespace Crpg.Domain.Entities.Notifications;
+
+public class NotificationMetadata
+{
+    public NotificationMetadata(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+    public int NotificationId { get; set; }
+    public string Key { get; set; }
+    public string Value { get; set; }
+}
