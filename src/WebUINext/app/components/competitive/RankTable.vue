abf77953809c8611d788fc45346116807da6be1c
Valeriy Sinevich <zohan33333@gmail.com>
2025-05-29 23:37:44 +0000

    new leaderboard

--- src/WebUINext/app/components/competitive/RankTable.vue
+++ src/WebUINext/app/components/competitive/RankTable.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import type { TableColumn } from '@nuxt/ui'
-
-import { tw } from '#imports'
+import { UTooltip } from '#components'
 import { groupBy, inRange } from 'es-toolkit'
 
 import type { Rank } from '~/models/competitive'
@@ -11,74 +9,43 @@ const { competitiveValue, rankTable } = defineProps<{
   rankTable: Rank[]
 }>()
 
-interface RankGroup {
-  title: string
-  rank1: Rank
-  rank2: Rank
-  rank3: Rank
-  rank4: Rank
-  rank5: Rank
-}
-
-const groupedRankTable = computed<RankGroup[]>(() =>
-  Object
-    .entries(groupBy(rankTable.toReversed(), r => r.groupTitle))
-    .map(([title, values]) => ({
-      title,
-      ...values.reduce((out, it, idx) => {
-        out[`rank${idx + 1}`] = it
-        return out
-      }, {}),
-    })))
-
-const columns: TableColumn<RankGroup>[] = [
-  {
-    accessorKey: 'title',
-    header: '',
-    cell: ({ row }) => h('span', { style: `color: ${row.original.rank1.color}` }, row.original.title),
-  },
-  ...[
-    'rank1',
-    'rank2',
-    'rank3',
-    'rank4',
-    'rank5',
-  ].reverse().map<TableColumn<RankGroup>>((it, idx) => ({
-    accessorFn: row => row[it],
-    header: String(idx + 1),
-    cell: ({ row }) => {
-      const rank = row.original[it]
-      return h('span', { style: `color: ${rank.color}` }, `${rank.min} - ${rank.max}`)
-    },
-  })),
-]
+const groupedRankTable = computed(() => groupBy(rankTable, r => r.groupTitle))
 </script>
 
 <template>
   <div class="space-y-8">
-    <UTable
-      class="relative rounded-md border border-muted"
-      :data="groupedRankTable"
-      :columns
-      :meta="{
-        class: {
-          tr: (row) => tw`text-[${row.original.rank1.color}]`,
-        },
-      }"
-    />
-
-    <!--
-    TODO: FIXME:
-      <span
-          v-if="
-            competitiveValue !== null
-              && inRange(competitiveValue, row[1][4 - idx].min, row[1][4 - idx].max)
-          "
-          :style="{ color: row[1][4 - idx].color }"
-          class="font-black"
+    <div class="flex flex-col-reverse gap-5 overflow-x-auto p-4">
+      <div
+        v-for="(ranks, groupTitle, groupIdx) in groupedRankTable"
+        :key="groupIdx"
+      >
+        <div
+          class="mb-1.5 text-xs font-bold"
+          :style="{
+            color: ranks.at(0)!.color,
+          }"
         >
-          {{ row[1][4 - idx].min }} - {{ row[1][4 - idx].max }} ({{ $t('you') }})
-        </span> -->
+          {{ groupTitle }} • {{ $n(ranks.at(0)!.min) }} – {{ $n(ranks.at(-1)!.max) }}
+        </div>
+
+        <div class="flex flex-col-reverse gap-1.5">
+          <UTooltip v-for="(rank, idx) in ranks" :key="idx" :text="`${rank.title} • ${$n(rank.min)} – ${$n(rank.max)}`">
+            <div
+              class="flex h-8 items-center px-2 text-2xs text-white select-none text-shadow-lg/20"
+              :style="{
+                backgroundColor: rank.color,
+                width: `calc(5rem + 0.5rem * ${groupIdx * ranks.length + idx + 1})`,
+              }"
+            >
+              {{ idx + 1 }} • {{ $n(rank.min) }} – {{ $n(rank.max) }}
+              <template v-if="competitiveValue && inRange(competitiveValue, rank.min, rank.max)">
+                ({{ $t('you') }} • {{ $n(competitiveValue) }})
+              </template>
+            </div>
+          </UTooltip>
+        </div>
+      </div>
+    </div>
 
     <div class="prose space-y-3 text-left prose-invert">
       <h5 class="text-content-100">
