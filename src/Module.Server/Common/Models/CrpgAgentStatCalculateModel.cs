dae0ce14b36a0e75c09841ccb031bbdf2d150043
Erdertus <racotsford@gmail.com>
2025-06-01 20:41:15 +0100

    Pre-test commit

--- src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
@@ -251,6 +251,29 @@ internal class CrpgAgentStatCalculateModel : AgentStatCalculateModel
         props.MountSpeed = (mount.GetModifiedMountSpeed(in mountHarness) + 1) * 0.209f * ridingImpactOnSpeed * weightImpactOnSpeed;
         props.TopSpeedReachDuration = Game.Current.BasicModels.RidingModel.CalculateAcceleration(in mount, in mountHarness, ridingSkill);
         props.MountDashAccelerationMultiplier = 1f / (2f + 8f * harnessWeightPercentage); // native between 1 and 0.1 . cRPG between 0.5 and 0.1
+
+        // Mounted penalty using same calculation as combat speed from strength and weapon length
+        if (agent.RiderAgent is Agent rider)
+        {
+            int strengthSkill = GetEffectiveSkill(rider, CrpgSkills.Strength);
+            EquipmentIndex weaponIndex = rider.GetWieldedItemIndex(Agent.HandIndex.MainHand);
+            WeaponComponentData? riderWeapon = weaponIndex != EquipmentIndex.None
+                ? rider.Equipment[weaponIndex].CurrentUsageItem
+                : null;
+
+            if (riderWeapon != null)
+            {
+                float weaponLengthPenalty = Math.Min(
+                    MBMath.Lerp(0.8f, 1f, MaxWeaponLengthForStrLevel(strengthSkill) / riderWeapon.WeaponLength),
+                    1f
+                );
+
+                props.MountManeuver *= weaponLengthPenalty;
+                props.MountSpeed *= weaponLengthPenalty;
+                props.MountDashAccelerationMultiplier *= weaponLengthPenalty;
+                props.MountChargeDamage *= weaponLengthPenalty;
+            }
+        }
     }
 
     // WARNING : for some reasone UpdateHumanAgentStats is called twice everytime there is a change (respawn or weapon switch)
