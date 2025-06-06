fd1c314c47f5b43e42b87052643a4128d0784d52
Yeldur <haotianyeldur@gmail.com>
2025-06-07 00:27:17 +0100

    Remove Xbox logon

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -129,31 +129,6 @@ builder.Services.AddOpenIddict()
             webProviderAdded = true;
         }
 
-        string? microsoftClientId = builder.Configuration["Microsoft:ClientId"];
-        string? microsoftClientSecret = builder.Configuration["Microsoft:ClientSecret"];
-
-        if (microsoftClientId != null && microsoftClientSecret != null)
-        {
-            webIntegrationBuilder.AddMicrosoft(microsoft =>
-            {
-                microsoft.SetClientId(microsoftClientId)
-                    .SetClientSecret(microsoftClientSecret)
-                    .SetRedirectUri("connect/callback-microsoft")
-                    // Use "consumers" once https://github.com/openiddict/openiddict-core/pull/1765 is released.
-                    .SetTenant("9188040d-6c67-4c5b-b112-36a304b66dad")
-                    .AddScopes("XboxLive.signin");
-            });
-
-            options.AddEventHandler(XboxUserInfoHandler.Descriptor);
-
-            webProviderAdded = true;
-        }
-
-        if (!webProviderAdded)
-        {
-            return;
-        }
-
         options.AllowAuthorizationCodeFlow();
 
         options.AddDevelopmentEncryptionCertificate()
