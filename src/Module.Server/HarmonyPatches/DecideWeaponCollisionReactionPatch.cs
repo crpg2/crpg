064d40f39baa328a46f120fe210bca5455e243a9
Harry Gardner <harrygardner123@gmail.com>
2025-11-10 10:48:52 +0000

    harmony fixes

--- src/Module.Server/HarmonyPatches/DecideWeaponCollisionReactionPatch.cs
+++ src/Module.Server/HarmonyPatches/DecideWeaponCollisionReactionPatch.cs
@@ -10,14 +10,15 @@ public class DecideWeaponCollisionReactionPatch
     [HarmonyPostfix]
 
     [HarmonyPatch(typeof(MissionCombatMechanicsHelper), "DecideWeaponCollisionReaction")]
-    public static void AddAxeSlicedThrough(in Blow registeredBlow,
+    public static void AddAxeSlicedThrough(
+        ref MeleeCollisionReaction colReaction,
+        in Blow registeredBlow,
         in AttackCollisionData collisionData,
         Agent attacker,
         Agent defender, in MissionWeapon attackerWeapon,
         bool isFatalHit,
         bool isShruggedOff,
-        float momentumRemaining,
-        out MeleeCollisionReaction colReaction)
+        float momentumRemaining)
     {
         // Check if the weapon used for the attack is an axe
         var weaponClass = attackerWeapon.IsEmpty ? WeaponClass.Undefined : attackerWeapon.CurrentUsageItem.WeaponClass;
@@ -25,13 +26,9 @@ public class DecideWeaponCollisionReactionPatch
         // Check if the attack hit an enemy (not a shield or an obstacle)
         bool hitEnemy = collisionData.IsColliderAgent && !collisionData.AttackBlockedWithShield;
         // Modify the collision reaction if the conditions are met
-        if (isAxe && hitEnemy)
+        if (isAxe && hitEnemy && colReaction != MeleeCollisionReaction.SlicedThrough)
         {
             colReaction = MeleeCollisionReaction.SlicedThrough;
-        }
-        else
-        {
-            colReaction = MeleeCollisionReaction.ContinueChecking;
         }
     }
 }
