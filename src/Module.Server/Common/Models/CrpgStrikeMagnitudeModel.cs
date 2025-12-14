211d81b78a400fb05b613dd3f35a88c84a259a39
Erdertus <racotsford@gmail.com>
2025-12-14 22:23:49 +0000

    Success?

--- src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
+++ src/Module.Server/Common/Models/CrpgStrikeMagnitudeModel.cs
@@ -21,59 +21,59 @@ internal class CrpgStrikeMagnitudeModel : MultiplayerStrikeMagnitudeModel
         _constants = constants;
     }
 
-    public override float GetBluntDamageFactorByDamageType(DamageTypes damageType)
-    {
-        float result = 0f;
-        switch (damageType)
-        {
-            case DamageTypes.Blunt:
-                result = 1f; // Native 1f
-                break;
-            case DamageTypes.Cut:
-                result = 0.35f; // Native .1f
-                break;
-            case DamageTypes.Pierce:
-                result = .45f; // Native .25f
-                break;
-        }
+    /* public override float GetBluntDamageFactorByDamageType(DamageTypes damageType)
+     {
+         float result = 0f;
+         switch (damageType)
+         {
+             case DamageTypes.Blunt:
+                 result = 1f; // Native 1f
+                 break;
+             case DamageTypes.Cut:
+                 result = 0.35f; // Native .1f
+                 break;
+             case DamageTypes.Pierce:
+                 result = .45f; // Native .25f
+                 break;
+         }
 
-        return result;
-    }
+         return result;
+     } */
 
-    public override float CalculateStrikeMagnitudeForSwing(
-        in AttackInformation attackInformation,
-        in AttackCollisionData collisionData,
-        in MissionWeapon weapon,
-        float swingSpeed,
-        float impactPoint,
-        float extraLinearSpeed)
-    {
-        float impactPointFactor;
-        float swingSpeedPercentage = swingSpeed * 4.5454545f / weapon.CurrentUsageItem.SwingSpeed; // should be replaced by attack progress , but needs validation
-        float extraLinearSpeedSign = Math.Sign(extraLinearSpeed);
-        float magnitudeBonusFromExtraSpeed = extraLinearSpeedSign * (float)(Math.Pow(extraLinearSpeedSign * extraLinearSpeed / 20f, 0.7f) + Math.Pow(extraLinearSpeed / 22f, 4f));
-        switch (weapon.CurrentUsageItem.WeaponClass)
-        {
-            case WeaponClass.OneHandedAxe:
-            case WeaponClass.TwoHandedAxe:
-            case WeaponClass.Mace:
-            case WeaponClass.TwoHandedMace:
-            case WeaponClass.OneHandedPolearm:
-            case WeaponClass.TwoHandedPolearm:
-            case WeaponClass.LowGripPolearm:
-                impactPointFactor = (float)Math.Pow(10f, -1.3f * Math.Pow(impactPoint - 0.85f, 2f));
-                return BladeDamageFactorToDamageRatio *
-                    (0.4f + 0.6f * impactPointFactor) *
-                    (float)(Math.Pow(swingSpeedPercentage, 5f) + magnitudeBonusFromExtraSpeed);
+    /* public override float CalculateStrikeMagnitudeForSwing(
+         in AttackInformation attackInformation,
+         in AttackCollisionData collisionData,
+         in MissionWeapon weapon,
+         float swingSpeed,
+         float impactPoint,
+         float extraLinearSpeed)
+     {
+         float impactPointFactor;
+         float swingSpeedPercentage = swingSpeed * 4.5454545f / weapon.CurrentUsageItem.SwingSpeed; // should be replaced by attack progress , but needs validation
+         float extraLinearSpeedSign = Math.Sign(extraLinearSpeed);
+         float magnitudeBonusFromExtraSpeed = extraLinearSpeedSign * (float)(Math.Pow(extraLinearSpeedSign * extraLinearSpeed / 20f, 0.7f) + Math.Pow(extraLinearSpeed / 22f, 4f));
+         switch (weapon.CurrentUsageItem.WeaponClass)
+         {
+             case WeaponClass.OneHandedAxe:
+             case WeaponClass.TwoHandedAxe:
+             case WeaponClass.Mace:
+             case WeaponClass.TwoHandedMace:
+             case WeaponClass.OneHandedPolearm:
+             case WeaponClass.TwoHandedPolearm:
+             case WeaponClass.LowGripPolearm:
+                 impactPointFactor = (float)Math.Pow(10f, -1.3f * Math.Pow(impactPoint - 0.85f, 2f));
+                 return BladeDamageFactorToDamageRatio *
+                     (0.4f + 0.6f * impactPointFactor) *
+                     (float)(Math.Pow(swingSpeedPercentage, 5f) + magnitudeBonusFromExtraSpeed);
 
-            default: // Weapon that do not have a wooden handle
-                impactPointFactor = (float)Math.Pow(10f, -1.5f * Math.Pow(impactPoint - 0.8f, 2f));
-                return BladeDamageFactorToDamageRatio
-                    * (0.7f + 0.3f * impactPointFactor)
-                    * (float)(Math.Pow(swingSpeedPercentage, 5f) + magnitudeBonusFromExtraSpeed);
-        }
-    }
-    public override float CalculateStrikeMagnitudeForThrust(
+             default: // Weapon that do not have a wooden handle
+                 impactPointFactor = (float)Math.Pow(10f, -1.5f * Math.Pow(impactPoint - 0.8f, 2f));
+                 return BladeDamageFactorToDamageRatio
+                     * (0.7f + 0.3f * impactPointFactor)
+                     * (float)(Math.Pow(swingSpeedPercentage, 5f) + magnitudeBonusFromExtraSpeed);
+         }
+     } */
+    /* public override float CalculateStrikeMagnitudeForThrust(
         in AttackInformation attackInformation,
         in AttackCollisionData collisionData,
         in MissionWeapon weapon,
@@ -95,34 +95,35 @@ internal class CrpgStrikeMagnitudeModel : MultiplayerStrikeMagnitudeModel
                  return BladeDamageFactorToDamageRatio *
                     (float)(Math.Pow(thrustSpeedPercentage, 2f) + magnitudeBonusFromExtraSpeed);
         }
-    }
+    } */
 
-    public override float CalculateAdjustedArmorForBlow(
-        in AttackInformation attackInformation,
-        in AttackCollisionData collisionData,
-        float baseArmor,
-        BasicCharacterObject attackerCharacter,
-        BasicCharacterObject attackerCaptainCharacter,
-        BasicCharacterObject victimCharacter,
-        BasicCharacterObject victimCaptainCharacter,
-        WeaponComponentData weaponComponent)
-    {
-        if (weaponComponent == null)
-        {
-            return baseArmor;
-        }
+    /*  public override float CalculateAdjustedArmorForBlow(
+          in AttackInformation attackInformation,
+          in AttackCollisionData collisionData,
+          float baseArmor,
+          BasicCharacterObject attackerCharacter,
+          BasicCharacterObject attackerCaptainCharacter,
+          BasicCharacterObject victimCharacter,
+          BasicCharacterObject victimCaptainCharacter,
+          WeaponComponentData weaponComponent)
+      {
+          if (weaponComponent == null)
+          {
+              return baseArmor;
+          }
 
-        return baseArmor * weaponComponent.WeaponClass switch
-        {
-            WeaponClass.Arrow => 1.2f,
-            WeaponClass.Bolt => 0.9f,
-            WeaponClass.Cartridge => 0.8f,
-            WeaponClass.Stone => 1.2f,
-            WeaponClass.Boulder => 0.9f,
-            WeaponClass.ThrowingAxe => 1.0f,
-            WeaponClass.ThrowingKnife => 1.0f,
-            WeaponClass.Javelin => 1.0f,
-            _ => 1f,
-        };
-    }
+          return baseArmor * weaponComponent.WeaponClass switch
+          {
+              WeaponClass.Arrow => 1.2f,
+              WeaponClass.Bolt => 0.9f,
+              WeaponClass.Cartridge => 0.8f,
+              WeaponClass.Stone => 1.2f,
+              WeaponClass.Boulder => 0.9f,
+              WeaponClass.ThrowingAxe => 1.0f,
+              WeaponClass.ThrowingKnife => 1.0f,
+              WeaponClass.Javelin => 1.0f,
+              _ => 1f,
+          };
+      }
+  } */
 }
