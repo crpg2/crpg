101bda8fed71bed1f17317b2eae573e89e381c9d
Valeriy Sinevich <zohan33333@gmail.com>
2026-03-07 20:30:37 +0000

    tweak char inv item detail

---
+++ src/WebUI/app/components/character/inventory/item/action/CharacterInventoryItemActionClanArmory.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { UserItem } from '~/models/user'
+
+import { useUser } from '~/composables/user/use-user'
+
+const {
+  userItem,
+} = defineProps<{
+  userItem: UserItem
+}>()
+
+defineEmits<{
+  add: []
+  remove: []
+  return: []
+}>()
+
+const { user } = useUser()
+
+const isOwnArmoryItem = computed(() => userItem.isArmoryItem && userItem.userId === user.value!.id)
+</script>
+
+<template>
+  <UTooltip
+    :text="!userItem.isArmoryItem
+      ? $t('clan.armory.item.add.title')
+      : isOwnArmoryItem
+        ? $t('clan.armory.item.remove.title')
+        : $t('clan.armory.item.return.title')"
+  >
+    <UButton
+      variant="subtle"
+      color="neutral"
+      size="xl"
+      block
+      icon="crpg:armory"
+      @click="!userItem.isArmoryItem
+        ? $emit('add')
+        : isOwnArmoryItem
+          ? $emit('remove')
+          : $emit('return')"
+    />
+  </UTooltip>
+</template>
