1d9096dc4af0d040ec2b54d8c338f79f6b1fb95d
Wasch Kadse <35863824+MrMendo@users.noreply.github.com>
2026-04-28 00:29:45 +0200

    mod: reduce xbow movespeed while reloading (#797)

--- src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
+++ src/Module.Server/Common/Models/CrpgAgentStatCalculateModel.cs
@@ -411,6 +411,9 @@ internal class CrpgAgentStatCalculateModel : AgentStatCalculateModel
                     props.WeaponRotationalAccuracyPenaltyInRadians /= ImpactOfStrReqOnCrossbows(agent, 0.3f, primaryItem);
                     props.ThrustOrRangedReadySpeedMultiplier *= 0.4f * (float)Math.Pow(2, weaponSkill / 191f) * ImpactOfStrReqOnCrossbows(agent, 0.3f, primaryItem); // Multiplying make windup time slower a 0 wpf, faster at 80 wpf
                     props.ReloadSpeed *= ImpactOfStrReqOnCrossbows(agent, 0.15f, primaryItem);
+
+                    // slow on reload (kicks in a bit late tho)
+                    props.BipedalRangedReloadSpeedMultiplier = 0.1f;
                 }
 
                 // Bows
